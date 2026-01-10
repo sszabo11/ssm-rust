b4112018b50671e1f25eb515c47b3e6223b4eb38
sszabo11 <sszabo1082@gmail.com>
2026-01-10 21:09:44 +1100

    rnn running

--- src/bin/net.rs
+++ src/bin/net.rs
@@ -4,14 +4,16 @@ use ndarray::Array1;
 use ssm::{
     embedding2::EmbeddingChar,
     rnn::{ForwardOutput, RNN},
-    utils::corpus_folder,
+    utils::{corpus_file, corpus_folder},
 };
 
 fn main() {
     let hidden_size = 100;
     let seq_length = 25;
 
-    let corpus = corpus_folder("./corpus/seuss");
+    //let corpus = corpus_folder("./corpus/seuss");
+
+    let corpus = corpus_file("./corpus/seuss/the-cat-in-a-hat.txt");
 
     const DIM: usize = 300;
     const SL_WIN: usize = 5;
@@ -21,28 +23,20 @@ fn main() {
 
     let mut rnn = RNN::new(hidden_size, seq_length, &corpus, DIM, SL_WIN, K);
 
-    let mut char_to_i: HashMap<char, usize> = HashMap::new();
-    let mut i_to_char: HashMap<usize, char> = HashMap::new();
-
-    let mut unique_chars: Vec<char> = corpus.chars().collect();
-    unique_chars.sort_unstable();
-    unique_chars.dedup();
-
-    for (idx, c) in unique_chars.into_iter().enumerate() {
-        char_to_i.insert(c, idx);
-        i_to_char.insert(idx, c);
-    }
-
     for sentence in corpus.split_terminator(".") {
         let tokens: Vec<char> = sentence.chars().collect();
 
         let idxs: Vec<usize> = tokens
             .iter()
-            .map(|c| *char_to_i.get(c).expect("Char not found"))
+            .map(|c| rnn.char_to_i.get(c).copied().expect("Char not found"))
             .collect();
+
+        println!("{:?}", idxs);
 
         rnn.train(Array1::from_vec(idxs), 10, 0.0001);
     }
+
+    rnn.inference("The", 100);
 
     //let ForwardOutput { pred_y: y, .. } = rnn.forward(Array1::zeros(rnn.vocab_size).view());
     //let ForwardOutput { pred_y: y2, .. } = rnn.forward(y.view());
--- src/rnn.rs
+++ src/rnn.rs
@@ -4,7 +4,7 @@ use ndarray::{Array, Array1, Array2, ArrayView, ArrayView1, Ix1, Ix2, ShapeArg};
 use ndarray_rand::RandomExt;
 use rand::distr::Uniform;
 
-use crate::embedding2::EmbeddingChar;
+use crate::embedding2::{EmbeddingChar, get_chars};
 
 pub struct RNN {
     #[allow(non_snake_case)]
@@ -26,6 +26,8 @@ pub struct RNN {
     seq_length: usize,
     //pub embedding: EmbeddingChar,
     pub vocab_size: usize,
+    pub char_to_i: HashMap<char, usize>,
+    pub i_to_char: HashMap<usize, char>,
 }
 
 pub struct ForwardOutput {
@@ -46,12 +48,25 @@ impl RNN {
         sliding_window: usize,
         k: usize,
     ) -> Self {
-        let vocab_size = corpus.len();
+        let vocab_size = get_chars(corpus, 1).len();
+        let mut char_to_i: HashMap<char, usize> = HashMap::new();
+        let mut i_to_char: HashMap<usize, char> = HashMap::new();
+
+        let mut unique_chars: Vec<char> = corpus.chars().collect();
+        unique_chars.sort_unstable();
+        unique_chars.dedup();
+
+        for (idx, c) in unique_chars.into_iter().enumerate() {
+            char_to_i.insert(c, idx);
+            i_to_char.insert(idx, c);
+        }
 
         Self {
             vocab_size,
             hidden_size,
             seq_length,
+            char_to_i,
+            i_to_char,
             embedding_dim,
             //embedding: EmbeddingChar::new(corpus.to_string(), embedding_dim, sliding_window, k),
             h: Array1::zeros(hidden_size),
@@ -76,31 +91,41 @@ impl RNN {
         }
     }
 
-    pub fn forward(&mut self, x_idx: usize, y_idx: usize) -> ForwardOutput {
-        let x = self.W_emb.row(x_idx);
-        let target_y_embedding = self.W_emb.row(y_idx);
+    pub fn forward(&mut self, input: Array1<usize>) -> Vec<ForwardOutput> {
+        let mut output: Vec<ForwardOutput> = Vec::with_capacity(input.len());
 
-        // Pre-Activation (logits or linear output)
-        let z_t = self.W_hh.dot(&self.h) + self.W_xh.dot(&x) + &self.bh;
+        for idx in input {
+            let x = self.W_emb.row(idx);
+            //println!("x {}", x.dim());
+            //println!("y {}", y_idx);
+            //
 
-        // Hidden state (This is activation a(L))
+            let y_idx = idx + 1;
+            let target_y_embedding = self.W_emb.row(y_idx);
 
-        //let a_t = z_t.tanh();
-        let a_t = z_t.map(|z| sigmoid(*z));
+            // Pre-Activation (logits or linear output)
+            let z_t = self.W_hh.dot(&self.h) + self.W_xh.dot(&x) + &self.bh;
 
-        self.h = a_t.clone();
+            // Hidden state (This is activation a(L))
 
-        // Prediction / output
-        let pred_y = self.W_hy.dot(&self.h) + &self.by;
+            //let a_t = z_t.tanh();
+            let a_t = z_t.mapv(sigmoid);
 
-        ForwardOutput {
-            h_t: a_t.clone(),
-            z_t,
-            pred_y,
-            a_t,
-            x_t: x.to_owned(),
-            target_y: target_y_embedding.to_owned(),
+            self.h = a_t.clone();
+
+            // Prediction / output
+            let pred_y = self.W_hy.dot(&self.h) + &self.by;
+
+            output.push(ForwardOutput {
+                h_t: a_t.clone(),
+                z_t,
+                pred_y,
+                a_t,
+                x_t: x.to_owned(),
+                target_y: target_y_embedding.to_owned(),
+            });
         }
+        output
     }
 
     // MSE
@@ -123,6 +148,42 @@ impl RNN {
         Array1::zeros(self.hidden_size)
     }
 
+    pub fn inference(&mut self, seed: &str, len: usize) -> String {
+        let mut response = String::new();
+
+        let last_char = seed.chars().last().unwrap();
+        let idx = self.char_to_i.get(&last_char).expect("Char not found");
+
+        while response.len() <= len {
+            let x = self.W_emb.row(*idx);
+
+            let z_t = self.W_hh.dot(&self.h) + self.W_xh.dot(&x) + &self.bh;
+
+            //let a_t = z_t.tanh();
+            let a_t = z_t.map(|z| sigmoid(*z));
+
+            self.h = a_t.clone();
+
+            // Prediction / output
+            let pred_y = self.W_hy.dot(&self.h) + &self.by;
+
+            println!("{}", pred_y.dim());
+
+            //for idx in pred_y.iter() {
+            //    println!("{}", self.i_to_char.get(&(*idx as usize)).unwrap());
+            //}
+
+            let c = self.i_to_char.get(&(pred_y[0] as usize)).unwrap();
+            response.push(*c);
+        }
+        //let final_logits = &outputs.last().unwrap();
+        //let probs = softmax(final_logits); // now probs sum to 1
+        //let next_char_idx = sample_from_probs(&probs); // e.g., weighted random or argmax
+        //let next_char = idx_to_char[next_char_idx];
+
+        response
+    }
+
     pub fn train(
         &mut self,
         input: Array1<usize>, // Sentence of chars as idxs to embedding
@@ -130,12 +191,21 @@ impl RNN {
         batch_size: usize,
         lr: f32,
     ) {
+        let mut loss = 0.0;
+        println!("{}", input.len());
         // x = idx of char of embedding
         for (i, x) in input.clone().into_iter().enumerate() {
-            // next idx of char of embedding
-            let target_y = input.get(i + 1).unwrap_or(&0);
+            if i >= input.len() - 1 {
+                continue;
+            }
 
-            //let target_y_embedding = self.W_emb.row(*target_y);
+            println!("Loss: {}", loss);
+            //println!("i: {} | L: {}", i, input.len());
+            let target = input[i + 1];
+            // next idx of char of embedding
+            let target_y = input.get(target).unwrap_or(&0);
+
+            let target_y_embedding = self.W_emb.row(*target_y);
 
             // Cache the predictions for backprop
             let mut pred_y_vec = Vec::new(); // Outputs of previous timesteps
@@ -146,38 +216,43 @@ impl RNN {
             let mut pred_real_y_vec = Vec::new(); // Target outputs previous timesteps
             //let mut pred_L_vec = Vec::new(); // Losses of previous timesteps
 
-            let mut total_gradient_W_xh = Array2::<f32>::zeros((self.hidden_size, self.vocab_size));
+            let mut total_gradient_W_xh =
+                Array2::<f32>::zeros((self.hidden_size, self.embedding_dim));
             let mut total_gradient_W_hh =
                 Array2::<f32>::zeros((self.hidden_size, self.hidden_size));
-            let mut total_gradient_W_hy = Array2::<f32>::zeros((self.vocab_size, self.hidden_size));
+            let mut total_gradient_W_hy =
+                Array2::<f32>::zeros((self.embedding_dim, self.hidden_size));
 
             //let mut total_gradient_by = Array1::zeros(self.vocab_size);
             //let mut total_gradient_bh = Array1::zeros(self.hidden_size);
 
             // Forward
-            for t in 0..input.len() {
-                self.clear_hidden();
+            //for t in 0..input.len() {
+            self.clear_hidden();
 
-                let f_o = self.forward(x, i + 1);
+            //if i >= input.len() {
+            //    continue 'forward;
+            //}
+            let f_o = self.forward(input);
 
-                //let loss = (&f_o.pred_y - target_y_embedding).pow2();
-                //pred_L_vec.push(loss);
+            //let l = (f_o.pred_y - target_y_embedding).pow2();
+            //pred_L_vec.push(loss);
 
-                // Cache for back pass
-                pred_y_vec.push(f_o.pred_y);
-                pred_z_vec.push(f_o.z_t);
+            // Cache for back pass
+            pred_y_vec.push(f_o.pred_y);
+            pred_z_vec.push(f_o.z_t);
 
-                // These are the same!! TODO: FIX
-                pred_h_vec.push(f_o.h_t);
-                pred_a_vec.push(f_o.a_t);
+            // These are the same!! TODO: FIX
+            pred_h_vec.push(f_o.h_t);
+            pred_a_vec.push(f_o.a_t);
 
-                pred_x_vec.push(f_o.x_t);
-                pred_real_y_vec.push(f_o.target_y);
-            }
+            pred_x_vec.push(f_o.x_t);
+            pred_real_y_vec.push(f_o.target_y);
+            //}
             let mut delta_h_next = Array1::zeros(self.hidden_size);
 
             // Backprop
-            for t in (0..self.seq_length).rev() {
+            for t in (0..input.len()).rev() {
                 let y_t = &pred_y_vec[t]; // Predicted Output
                 let z_t = &pred_z_vec[t]; // Pre-Activation
                 let h_t = &pred_h_vec[t]; // Hidden state
@@ -189,8 +264,8 @@ impl RNN {
                 let sig_der = z_t.map(|z| sigmoid_der(*z)); // Sigmoid derivative element wise
                 let delta_y = y_t - target_y; // derivative dL/da(L) or dL/dy
 
+                loss = delta_y.sum() / delta_y.len() as f32;
                 let delta_h = self.W_hy.t().dot(&delta_y) + &delta_h_next;
-                println!("{} {}", delta_y.dim(), sig_der.dim());
                 let delta_z = &delta_h * sig_der; // derivative dL/da(L) or dL/dy
 
                 {
@@ -248,7 +323,7 @@ impl RNN {
 
 //fn calculate_weight(target_y: Array1<f32>, pred_y: Array1<f32>) -> Array1<f32> {}
 
-pub fn outer_product(x: &Array<f64, Ix1>, y: &Array<f64, Ix1>) -> Array<f64, Ix2> {
+pub fn outer_product(x: &Array<f32, Ix1>, y: &Array<f32, Ix1>) -> Array<f32, Ix2> {
     let (size_x, size_y) = (x.shape()[0], y.shape()[0]);
     let x_view = x.view();
     let y_view = y.view();
@@ -257,12 +332,12 @@ pub fn outer_product(x: &Array<f64, Ix1>, y: &Array<f64, Ix1>) -> Array<f64, Ix2
     x_reshaped.dot(&y_reshaped)
 }
 
-fn softmax(nums: Vec<f32>) -> Vec<f32> {
+fn softmax(nums: &Array1<f32>) -> Array1<f32> {
     let sum: f64 = nums.iter().map(|n| f64::consts::E.powf(*n as f64)).sum();
 
     nums.iter()
         .map(|n| (f64::consts::E.powf(*n as f64) / sum) as f32)
-        .collect::<Vec<f32>>()
+        .collect::<Array1<f32>>()
 }
 
 fn sigmoid_der(x: f32) -> f32 {
@@ -277,9 +352,9 @@ mod tests {
 
     #[test]
     fn test_softmax() {
-        let nums = vec![-1.0, 0.0, 3.0, 5.0];
+        let nums = array![-1.0, 0.0, 3.0, 5.0];
 
-        let soft = softmax(nums);
+        let soft = softmax(&nums);
 
         assert_eq!(soft.iter().sum::<f32>(), 1.0);
         assert_eq!(soft[0], 0.00216569646);
@@ -298,6 +373,10 @@ mod tests {
 }
 fn sigmoid(x: f32) -> f32 {
     1.0 / (1.0 + (-x).exp())
+}
+fn cross_entropy_loss(logits: &Array1<f32>, target_idx: usize) -> f32 {
+    let probs = softmax(logits); // you implement softmax
+    -probs[target_idx].ln() // negative log prob of true class
 }
 
 //fn outer_product<'a>(x: &'a Array1<f32>, y: &'a Array1<f32>) -> Array2<f32> {
